44d78943e4c84d5e488fd16937283402fd44944f
bayuirfan52 <bayuirfan52@gmail.com>
2023-08-21 07:30:02 +0700

    feat: initialize getx environment

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
-import '../modules/home/bindings/home_binding.dart';
-import '../modules/home/views/home_view.dart';
+import '../modules/home/home_binding.dart';
+import '../modules/home/home_view.dart';
 
 part 'app_routes.dart';
 
@@ -11,6 +11,11 @@ class AppPages {
   static const INITIAL = Routes.HOME;
 
   static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Andin Project",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+Future<void> main() async {
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await FlutterConfig.loadEnvVariables();
+    await GetStorage.init();
+    runApp(const AndinProject());
+  }, (error, stack) {});
+}
+
+class AndinProject extends StatelessWidget {
+  const AndinProject({super.key});
+
+  @override
+  Widget build(BuildContext context) => GetMaterialApp(
+        title: 'Andin Project',
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+        defaultTransition: Transition.cupertino,
+      );
 }
