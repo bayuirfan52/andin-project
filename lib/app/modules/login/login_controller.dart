df024de49761be662e1905bcd63dac7cd73fa521
bayuirfan52 <bayuirfan52@gmail.com>
2023-08-21 08:08:56 +0700

    feat: login page

---
+++ lib/app/modules/login/login_controller.dart
@@ -0,0 +1,18 @@
+import 'package:andin_project/app/core/base/base_controller.dart';
+
+class LoginController extends BaseController {
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import '../modules/home/home_binding.dart';
 import '../modules/home/home_view.dart';
+import '../modules/login/login_binding.dart';
+import '../modules/login/login_view.dart';
 
 part 'app_routes.dart';
 
@@ -17,9 +19,9 @@ class AppPages {
       binding: HomeBinding(),
     ),
     GetPage(
-      name: _Paths.HOME,
-      page: () => const HomeView(),
-      binding: HomeBinding(),
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
