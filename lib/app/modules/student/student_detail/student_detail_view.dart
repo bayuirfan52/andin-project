c46c6f6ebfd4b9d84b2749466fcbdd649065df13
bayuirfan52 <bayuirfan52@gmail.com>
2023-09-11 15:00:30 +0700

    fix: optimize import

--- lib/app/modules/student/student_detail/student_detail_view.dart
+++ lib/app/modules/student/student_detail/student_detail_view.dart
@@ -1,7 +1,5 @@
-import 'package:andin_project/app/core/resources/app_color.dart';
 import 'package:andin_project/app/widgets/ex_dialog.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:velocity_x/velocity_x.dart';
